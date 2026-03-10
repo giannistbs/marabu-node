1b1f28e0f9a6bbafcab2f343698a6ee2d40e8699
giannistbsnet <itampaki@netcompany.com>
2026-03-10 09:38:14 +0100

    Add comments to the hashing

--- src/hashing.ts
+++ src/hashing.ts
@@ -2,8 +2,12 @@ import { ApplicationObject } from "./types.js";
 import { encodeApplicationObject } from "./codec.js";
 import blake2 from "blake2";
 
+// Computes the object ID of an application object by canonicalizing and hashing the object.
 export function computeObjectId(object: ApplicationObject): string {
+  // Create a new Blake2s hash object.
   const h = blake2.createHash("blake2s");
+  // Update the hash object with the encoded(canonicalized) application object.
   h.update(Buffer.from(encodeApplicationObject(object)));
+  // Return the hash digest as a hexadecimal string.
   return h.digest("hex");
 }
