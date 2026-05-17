9052ba1b18609dbd758b8a89490b54a94f101328
giannistbs <giannistampakis04@gmail.com>
2026-05-17 22:51:47 +0300

    Implement worker-based mining

--- src/mining/mine.ts
+++ src/mining/mine.ts
@@ -1,13 +1,15 @@
-import * as ed from "@noble/ed25519";
-import { sha512 } from "@noble/hashes/sha2.js";
-import { encodeTransactionSigningPayload } from "../protocol/codec.js";
+import { Worker } from "node:worker_threads";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
 import {
   GENESIS_BLOCK,
   type Block,
 } from "../types.js";
 import { computeObjectId } from "../protocol/hashing.js";
 
-const REQUIRED_BLOCK_TARGET =
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export const REQUIRED_BLOCK_TARGET =
   "00000000abc00000000000000000000000000000000000000000000000000000";
 const BLOCK_REWARD = 50_000_000_000_000n;
 const GENESIS_BLOCK_ID = computeObjectId(GENESIS_BLOCK);
@@ -15,30 +17,36 @@ const MISSING_BLOCK_TX_WAIT_MS = 3_500;
 const MISSING_BLOCK_WAIT_MS = 4_500;
 
 
+let activeWorkers: Worker[] = [];
+
 // Number of workers will be derived from env variables to be set depending on the machine
 export async function spawnMiningWorkers(block: Block, numOfWorkers: number): Promise<Block> {
 
-  // here we should kill all the existing workers and spawn new ones
+  // kill all existing workers before spawning new ones
+  for (const worker of activeWorkers) {
+    worker.terminate();
+  }
+  activeWorkers = [];
 
-    return workers.spawn(numOfWorkers, mineBlock(block, numOfWorkers))
-}
+  const workerPromises: Promise<Block>[] = [];
+  for (let i = 0; i < numOfWorkers; i++) {
+    const worker = new Worker(join(__dirname, "mineWorker.ts"), {
+      execArgv: ["--import", "tsx"],
+      workerData: { block: { ...block }, step: numOfWorkers, workerSlot: i }
+    });
+    activeWorkers.push(worker);
+    workerPromises.push(new Promise<Block>((resolve, reject) => {
+      worker.on("message", resolve);
+      worker.on("error", reject);
+    }));
+  }
 
+  const winner = await Promise.race(workerPromises);
 
+  for (const worker of activeWorkers) {
+    worker.terminate();
+  }
+  activeWorkers = [];
 
-export async function mineBlock(block: Block, step: number, workerSlot: number): Promise<Block> {
-
-    const targetValue = BigInt(`0x${REQUIRED_BLOCK_TARGET}`);
-    let nonce = BigInt(workerSlot % step);
-
-    // run an infinite loop that alters block.nonce and only returns if computeObjectId(block) < T
-    while (true) {
-        block.nonce = nonce.toString(16).padStart(64, "0");
-        nonce = nonce + BigInt(step);
-        const blockId = computeObjectId(block);
-        const blockValue = BigInt(`0x${blockId}`);
-        if (blockValue < targetValue) {
-          // Kill all other workers
-          return block;
-        }
-    }
+  return winner;
 }
